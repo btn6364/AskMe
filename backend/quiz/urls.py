d54673d87d2a0237a26fd4fa8fc9d50ac81a0536
btn6364@rit.edu <btn6364@rit.edu>
2020-12-16 19:34:41 -0500

    Add the default URL

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from quiz import views
 
 urlpatterns = [
+    path('', views.QuizList.as_view()),
     path('quizzes/', views.QuizList.as_view()),
     path('quizzes/<int:pk>/', views.QuizDetail.as_view()), 
     path('topics/', views.TopicList.as_view()),
